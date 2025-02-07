33a41edd525c4cd63c7b1414b891979ff4fb80a5
Adam Bender <abender@gmail.com>
2025-02-07 13:48:40 -0800

    Update red to print the entire width and have configurable height

--- red/main.go
+++ red/main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/fatih/color"
+	"golang.org/x/term"
 )
 
 func main() {
-	red := color.New(color.BgHiRed)
-	out := ""
-	for range 20 {
-		for range 120 {
-			out += red.Sprint(" ")
-		}
-		out += "\n"
+	height := 20
+	width := 120
+
+	x, _, err := term.GetSize(int(os.Stdin.Fd()))
+	if err != nil {
+		fmt.Println("GetSize:", err)
+	} else {
+		width = x
 	}
-	fmt.Print(out)
+
+	if len(os.Args) == 2 {
+		y, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Printf("%q is not a number: %v", os.Args[1], err)
+		} else {
+			height = y
+		}
+	}
+
+	out := strings.Repeat(" ", width)
+	out += strings.Repeat("\n", height-1)
+	color.New(color.BgHiRed).Println(out)
 }
